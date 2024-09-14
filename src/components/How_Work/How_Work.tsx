5782c61420564fd144ba2c0e9414e0c3b0c04e25
Bhawana121999 <bhawana.12.bhatt@gmail.com>
2024-09-14 09:53:21 -0700

    add chnages

--- src/components/How_Work/How_Work.tsx
+++ src/components/How_Work/How_Work.tsx
@@ -26,71 +26,72 @@ export default function How_Work() {
                     </div>
 
 
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <div className={`row mt-4 pt-5 justify-content-center align-items-center ${style.rows}`}>
+                            <div className="col-lg-7 col-md-7 col-sm-12">
+                                <div className={style.box}>
+                                    <h6 className={style.step}>1</h6>
+                                    <div className={style.main_content}>
+                                        <h4 className={style.head}>Share your requirements</h4>
+                                        <p className={style.para1}>Tell us your event date, budget, location, type of venue, guest count, etc.</p>
+                                    </div>
+                                </div>
 
-                    <div className={`row mt-4 pt-5 justify-content-center align-items-center ${style.rows}`}>
-                        <div className="col-lg-7 col-md-7 col-sm-7">
-                            <div className={style.box}>
-                                <h6 className={style.step}>1</h6>
-                                <div className={style.main_content}>
-                                    <h4 className={style.head}>Share your requirements</h4>
-                                    <p className={style.para1}>Tell us your event date, budget, location, type of venue, guest count, etc.</p>
+                                <div className={style.box}>
+                                    <h6 className={style.step}>2</h6>
+                                    <div className={style.main_content}>
+                                        <h4 className={style.head}>Get a personalised proposal</h4>
+                                        <p className={style.para1}>Get the best deals on venue, catering, and decor as per your preferences.</p>
+                                    </div>
+                                </div>
+
+                                <div className={style.box}>
+                                    <h6 className={style.step}>3</h6>
+                                    <div className={style.main_content}>
+                                        <h4 className={style.head}>Confirm and book</h4>
+                                        <p className={style.para1}>Pay a minimum amount & lock the deal within 7 days. Leave the rest to us.</p>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className={style.box}>
-                                <h6 className={style.step}>2</h6>
-                                <div className={style.main_content}>
-                                    <h4 className={style.head}>Get a personalised proposal</h4>
-                                    <p className={style.para1}>Get the best deals on venue, catering, and decor as per your preferences.</p>
-                                </div>
+                            <div className='col-lg-5 col-md-5 col-sm-12'>
+                                <Swiper
+                                    loop={true}
+                                    rewind={false}
+                                    slidesPerView={1}
+                                    spaceBetween={30}
+                                    // autoplay={{
+                                    //     delay: 3000,
+                                    //     disableOnInteraction: false,
+                                    // }}
+                                    // modules={[Autoplay]}
+                                    className={`mySwiper ${style.background_img}`}
+                                >
+
+                                    <SwiperSlide className={style.slide}>
+                                        <h6 className={style.number}>1</h6>
+                                        <div className={style.image}>
+                                            <Image src={step1} alt='' fill priority />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className={style.slide}>
+                                        <h6 className={style.number}>2</h6>
+                                        <div className={style.image}>
+                                            <Image src={step2} alt='' fill priority />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className={style.slide}>
+                                        <h6 className={style.number}>3</h6>
+                                        <div className={style.image}>
+                                            <Image src={step3} alt='' fill priority />
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+
                             </div>
-
-                            <div className={style.box}>
-                                <h6 className={style.step}>3</h6>
-                                <div className={style.main_content}>
-                                    <h4 className={style.head}>Confirm and book</h4>
-                                    <p className={style.para1}>Pay a minimum amount & lock the deal within 7 days. Leave the rest to us.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='col-lg-5 col-md-5 col-sm-5'>
-                            <Swiper
-                                loop={true}
-                                rewind={false}
-                                slidesPerView={1}
-                                spaceBetween={30}
-                                // autoplay={{
-                                //     delay: 3000,
-                                //     disableOnInteraction: false,
-                                // }}
-                                // modules={[Autoplay]}
-                                className={`mySwiper ${style.background_img}`}
-                            >
-
-                                <SwiperSlide className={style.slide}>
-                                    <h6 className={style.number}>1</h6>
-                                    <div className={style.image}>
-                                        <Image src={step1} alt='' fill priority />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={style.slide}>
-                                    <h6 className={style.number}>2</h6>
-                                    <div className={style.image}>
-                                        <Image src={step2} alt='' fill priority />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={style.slide}>
-                                    <h6 className={style.number}>3</h6>
-                                    <div className={style.image}>
-                                        <Image src={step3} alt='' fill priority />
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
-
                         </div>
                     </div>
-
+                    
                 </div>
             </div>
         </div>
